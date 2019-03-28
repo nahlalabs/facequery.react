1453036100310c66fcaad47df9c42a76c3bdfd75
ahadsenja <hamdanilm96@gmail.com>
2019-03-28 21:39:42 +0800

    refactor: change App.js code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavbarHeader from './component/Header/Navbar'
+import JumbotronHeader from './component/Header/Jumbotron'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header></Header>
       </div>
     );
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <NavbarHeader></NavbarHeader>
+        <JumbotronHeader></JumbotronHeader>
+      </div>
+    )
   }
 }
 
